773aa40f1d6aa856c3c6a5457994e4ed588d1dd0
elizabethkeira01-lang <elizabeth.keira01@gmail.com>
2026-06-23 02:47:37 -0600

    Conectar supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,8 @@
 const SUPABASE_URL = "https://tvmftquerbehaqyavjib.supabase.co";
+
 const SUPABASE_ANON_KEY = "sb_publishable_ZclPBgdOhTsZFWaudYA5Iw_CABHGT2h";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
