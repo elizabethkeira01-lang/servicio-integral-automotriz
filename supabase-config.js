34e729530aac19232cbabe1a84c2b3c00699c8c6
elizabethkeira01-lang <elizabeth.keira01@gmail.com>
2026-06-23 03:13:38 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://tvmftquerbehaqyavjib.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_ZclPBgdOhTsZFWaudYA5Iw_CABHGT2h";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2bWZ0cXVlcmJlaGFxeWF2amliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxODA4NTgsImV4cCI6MjA5Nzc1Njg1OH0.5_FzF5C55bjbm5oMo6i2QPbyAlgGt4MOkLw0rLBsvwA";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
